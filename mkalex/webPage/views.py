12e0f7549cc42b4eebbf944649318d9a03c8aaff
MikiAlex <18516340862@163.com>
2017-01-24 20:00:34 +0800

    add basic blog getting support

--- mkalex/webPage/views.py
+++ mkalex/webPage/views.py
@@ -6,6 +6,12 @@ from django.template import loader
 
 from .models import Article,Recommended_article
 
+from markdown2 import Markdown
+markdowner = Markdown()
+blog_assets_base='/static/image/blog/'
+def set_url(markdown_text):
+    return markdown_text.replace("{#base#}",blog_assets_base)
+
 def index(request):
     template=loader.get_template('webPage/index.jinja')
     art=get_object_or_404(Article,pk=1)
@@ -16,7 +22,7 @@ def index(request):
     return HttpResponse(template.render(content))
 
 
-
+#blog home page
 def blog(request):
     template=loader.get_template('webPage/blog.jinja')
     article_list = Article.objects.all()
@@ -28,7 +34,38 @@ def blog(request):
     }
     return HttpResponse(template.render(content))
 
-#blog home page
+
+
+def blog_detail(request,id):
+    template=loader.get_template('webPage/blog_detail.jinja')
+    article = Article.objects.get(pk=id);
+    # Recommended_article_list=Recommended_article.objects.all()
+    content_html=markdowner.convert(set_url(article.content))
+    content={
+        'page_title':'Blog',
+        # 'list':article_list,
+        'content':content_html,
+    }
+    return HttpResponse(template.render(content))
+
+
+
+
+def blog_year_archive(request,year):
+    pass
+    # template=loader.get_template('webPage/blog_detail.jinja')
+    # article_list = Article.objects.all()
+    # # Recommended_article_list=Recommended_article.objects.all()
+    # content={
+    #     'page_title':'jkjkjkjkjkjkjkjkjkjk',
+    #     # 'list':article_list,
+    #     # 'rec_list':Recommended_article_list,
+    # }
+    # return HttpResponse(template.render(content))
+    # return HttpResponse(template.render(content))
+
+
+
 def portfolio(request):
     template=loader.get_template('webPage/portfolio.jinja')
     content={
