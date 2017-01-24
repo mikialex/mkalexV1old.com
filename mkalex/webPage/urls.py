ff9104388dfe18a7863c694c26a482280f87f5fc
MikiAlex <18516340862@163.com>
2017-01-25 01:34:25 +0800

    add support for right_col

--- mkalex/webPage/urls.py
+++ mkalex/webPage/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^blog$', views.blog, name='blog'),
     url(r'^blog/year/(?P<year>[0-9]{4})/$', views.blog_year_archive, name='blog_year_archive'),
+    url(r'^blog/category/(?P<category_id>[0-9]+)/$', views.blog_category_archive, name='blog_category_archive'),
     url(r'^blog/(?P<id>[0-9]+)/$', views.blog_detail, name='blog_detail'),
     url(r'^portfolio$', views.portfolio, name='portfolio'),
     url(r'^archive$', views.archive, name='archive'),
--- mkalex/webPage/views.py
+++ mkalex/webPage/views.py
@@ -10,13 +10,24 @@ from .models import Article,Recommended_article,Category
 from markdown2 import Markdown
 markdowner = Markdown()
 blog_assets_base='/static/image/blog/'
+
 def set_url(markdown_text):
     return markdown_text.replace("{#base#}",blog_assets_base)
 
 def get_right_col():
+
+    category_list=Category.objects.all()
+    def count_category_num(category):
+        return {'name':category.name,
+        'num':Article.objects.filter(category_id=category.id).count(),
+        'id':category.id}
+    categoryAll=map(count_category_num,category_list)
+
     right_col_data={
+        'category':categoryAll,
         'num2016':Article.objects.filter(publish_time__year=2016).count(),
         'num2017':Article.objects.filter(publish_time__year=2017).count(),
+        'num_all':Article.objects.all().count(),
         'Recommended_article_list':map(lambda x:Article.objects.get(pk=x.article_id),Recommended_article.objects.all())
     }
     return right_col_data
@@ -58,11 +69,9 @@ def blog(request):
 def blog_detail(request,id):
     template=loader.get_template('webPage/blog_detail.jinja')
     article = Article.objects.get(pk=id);
-    # Recommended_article_list=Recommended_article.objects.all()
     content_html=markdowner.convert(set_url(article.content))
     content={
         'page_title':'Blog',
-        # 'list':article_list,
         'content':content_html,
         'right_col_data':get_right_col(),
     }
@@ -72,17 +81,31 @@ def blog_detail(request,id):
 
 
 def blog_year_archive(request,year):
-    pass
-    # template=loader.get_template('webPage/blog_detail.jinja')
-    # article_list = Article.objects.all()
-    # # Recommended_article_list=Recommended_article.objects.all()
-    # content={
-    #     'page_title':'jkjkjkjkjkjkjkjkjkjk',
-    #     # 'list':article_list,
-    #     # 'rec_list':Recommended_article_list,
-    # }
-    # return HttpResponse(template.render(content))
-    # return HttpResponse(template.render(content))
+    template=loader.get_template('webPage/blog.jinja')
+    article_list = Article.objects.filter(publish_time__year=year)
+    content={
+        'page_title':'Blog in '+year,
+        'list':article_list,
+        'right_col_data':get_right_col(),
+    }
+    return HttpResponse(template.render(content))
+    return HttpResponse(template.render(content))
+
+
+
+def blog_category_archive(request,category_id):
+    template=loader.get_template('webPage/blog.jinja')
+    article_list = Article.objects.filter(category_id=category_id)
+    category_name=Category.objects.get(id=category_id).name
+    content={
+        'page_title':'Blog 的 '+ category_name+' 子分类',
+        'list':article_list,
+        'right_col_data':get_right_col(),
+    }
+    return HttpResponse(template.render(content))
+    return HttpResponse(template.render(content))
+
+
 
 
 
